3109bdb0da0dd9bd4a940772182953d45e3aa260
Wd Games <67733346+891-2@users.noreply.github.com>
2022-05-30 21:55:22 +0500

    Update alpha.user.js

--- alpha.user.js
+++ alpha.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VK By RAM
 // @namespace    https://github.com/891-2/vk-old-rad/
-// @version      3.8.1
+// @version      3.8.2
 // @description  Вернём старый дизайн вместе
 // @author       RAM
 // @match        *://*.vk.com/*
