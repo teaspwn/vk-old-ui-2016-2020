45ec181f977bbddd6c46770d6308fcf28856a604
RemoteCaller <93471240+RemoteCaller-Exiled@users.noreply.github.com>
2022-08-17 19:24:26 +0800

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VK By Tresha
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/RemoteCaller-Exiled/vk-old-2021
+// @version      0.2
 // @description  Дизайн вк за 2021 год
 // @author       Tresha
 // @match        *://*.vk.com/*
@@ -143,7 +143,7 @@ div.VideoCard__additionalInfo.video_item_add_info {
     content: 'Понравилось';
     visibility: visible;
   }
-
+/* Старые иконки */
 [dir] .like_wrap.lite .like_btn.like .like_button_icon {
     background-image: url(data:image/svg+xml;charset=utf-8,%3Csvg%20height%3D%229%22%20width%3D%2211%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M7.9%200C9.612%200%2011%201.36%2011%203.039c0%201.688-.69%202.534-3.597%204.75L6.063%208.81a.931.931%200%2001-1.126%200l-1.34-1.022C.69%205.573%200%204.727%200%203.039%200%201.36%201.388%200%203.1%200c.904%200%202.4.9%202.4%202.25C5.5.9%206.996%200%207.9%200z%22%20fill%3D%22%23828a99%22%2F%3E%3C%2Fsvg%3E);
 }
@@ -154,11 +154,178 @@ div.VideoCard__additionalInfo.video_item_add_info {
     width: 11px;
     height: 9px;
 }
+
 #l_vid > .left_row > .LeftMenu__icon > svg path {
     d: path('M12.06 1.5c.65 0 1.23 0 1.7.04.51.04 1.04.13 1.56.4a4 4 0 011.74 1.74c.27.52.36 1.05.4 1.56.04.47.04 1.05.04 1.7v6.12c0 .65 0 1.23-.04 1.7a4.07 4.07 0 01-.4 1.56 4 4 0 01-1.74 1.74c-.52.27-1.05.36-1.56.4-.47.04-1.05.04-1.7.04H7.94c-.65 0-1.23 0-1.7-.04a4.06 4.06 0 01-1.56-.4 4 4 0 01-1.74-1.74 4.06 4.06 0 01-.4-1.56c-.04-.47-.04-1.05-.04-1.7V6.94c0-.65 0-1.23.04-1.7.04-.51.13-1.04.4-1.56a4 4 0 011.74-1.74 4.06 4.06 0 011.56-.4c.47-.04 1.05-.04 1.7-.04zM4.27 4.37C4 4.9 4 5.6 4 7v6c0 1.4 0 2.1.27 2.63.24.48.62.86 1.1 1.1C5.9 17 6.6 17 8 17h4c1.4 0 2.1 0 2.63-.27a2.5 2.5 0 001.1-1.1C16 15.1 16 14.4 16 13V7c0-1.4 0-2.1-.27-2.63a2.5 2.5 0 00-1.1-1.1C14.1 3 13.4 3 12 3H8c-1.4 0-2.1 0-2.63.27a2.5 2.5 0 00-1.1 1.1zM6 14c0-.33 0-.49.06-.62a.65.65 0 01.32-.32c.13-.06.3-.06.62-.06s.49 0 .62.06c.14.06.26.18.32.32.06.13.06.3.06.62s0 .49-.06.62a.65.65 0 01-.32.32c-.13.06-.3.06-.62.06s-.49 0-.62-.06a.65.65 0 01-.32-.32C6 14.49 6 14.32 6 14zm.06-4.62C6 9.51 6 9.68 6 10c0 .33 0 .49.06.62.06.14.18.26.32.32.13.06.3.06.62.06s.49 0 .62-.06a.65.65 0 00.32-.32c.06-.13.06-.3.06-.62 0-.33 0-.49-.06-.62a.65.65 0 00-.32-.32C7.49 9 7.32 9 7 9s-.49 0-.62.06a.65.65 0 00-.32.32zM12 6c0-.33 0-.49.06-.62a.65.65 0 01.32-.32c.13-.06.3-.06.62-.06s.49 0 .62.06c.14.06.26.18.32.32.06.13.06.3.06.62s0 .49-.06.62a.65.65 0 01-.32.32c-.13.06-.3.06-.62.06s-.49 0-.62-.06a.65.65 0 01-.32-.32C12 6.49 12 6.32 12 6zm.06 7.38c-.06.13-.06.3-.06.62s0 .49.06.62c.06.14.18.26.32.32.13.06.3.06.62.06s.49 0 .62-.06a.65.65 0 00.32-.32c.06-.13.06-.3.06-.62s0-.49-.06-.62a.65.65 0 00-.32-.32c-.13-.06-.3-.06-.62-.06s-.49 0-.62.06a.65.65 0 00-.32.32zM12 10c0-.33 0-.49.06-.62a.65.65 0 01.32-.32c.13-.06.3-.06.62-.06s.49 0 .62.06c.14.06.26.18.32.32.06.13.06.3.06.62 0 .33 0 .49-.06.62a.65.65 0 01-.32.32c-.13.06-.3.06-.62.06s-.49 0-.62-.06a.65.65 0 01-.32-.32c-.06-.13-.06-.3-.06-.62zM6.06 5.38C6 5.51 6 5.68 6 6s0 .49.06.62c.06.14.18.26.32.32.13.06.3.06.62.06s.49 0 .62-.06a.65.65 0 00.32-.32C8 6.49 8 6.32 8 6s0-.49-.06-.62a.65.65 0 00-.32-.32C7.49 5 7.32 5 7 5s-.49 0-.62.06a.65.65 0 00-.32.32z') !important;
 }
 #l_aud > .left_row > .LeftMenu__icon > svg path {
     d: path('M14.73 2.05a2.28 2.28 0 012.75 2.23v7.99c0 3.57-3.5 5.4-5.39 3.51-1.9-1.9-.06-5.38 3.52-5.38h.37V6.76L8 8.43v5.82c0 3.5-3.35 5.34-5.27 3.62l-.11-.1c-1.9-1.9-.06-5.4 3.51-5.4h.37V6.24c0-.64.05-1 .19-1.36l.05-.13c.17-.38.43-.7.76-.93.36-.26.7-.4 1.41-.54zM6.5 13.88h-.37c-2.32 0-3.34 1.94-2.45 2.82.88.89 2.82-.13 2.82-2.45v-.37zm9.48-1.98h-.37c-2.32 0-3.34 1.94-2.46 2.82.89.89 2.83-.13 2.83-2.45v-.37zm-.02-7.78a.78.78 0 00-.92-.6L9.06 4.77c-.4.09-.54.15-.68.25a.8.8 0 00-.27.33c-.08.18-.1.35-.1.88v.67l7.97-1.67v-.95-.08z') !important;
+}
+/* Реакции */
+.ReactionsMenu, .ReactionsMenu--extraHoverArea, .ReactionsMenu--extraHoverAreaToTop, div.ReactionsPreview__items
+  {
+    display: none;
+  }
+
+  .PostBottomAction
+  {
+    --post-bottom-action-background-color: transparent !important;
+  }
+
+  [dir=ltr] .PostButtonReactions--post .PostButtonReactions__title--textual
+  {
+    display: none;
+  }
+
+  [dir=ltr] .like_tt_reacted-count
+  {
+    display: none;
+  }
+
+  div.ReactionsPreview.ReactionsPreview--active .ReactionsPreview__count._counter_anim_container
+  {
+    color: #e64646 !important;
+  }
+
+  [dir] .ReactionsPreview
+  {
+    position: absolute !important;
+    margin-top: 28px;
+    margin-left: 30px;
+    width: 50px;
+    z-index: 100;
+  }
+
+  .PostBottomAction__count, .PostBottomAction__label, [dir=ltr] .ReactionsPreview__count
+  {
+    font-weight: 500 !important;
+    font-size: 14px !important;
+    color: #5e6c77;
+  }
+
+  .fans_fanph_reaction
+  {
+    display: none !important;
+  }
+
+  li#likes_tab_reactions_0, li#likes_tab_reactions_1, li#likes_tab_reactions_2, li#likes_tab_reactions_3, li#likes_tab_reactions_4, li#likes_tab_reactions_5,.ui_tab.ui_tab_group
+  {
+    display: none;
+  }
+
+  .like_tt_reaction
+  {
+    display: none !important;
+  }
+
+  .PostButtonReactionsContainer
+  {
+    width: auto !important;
+  }
+
+  [dir] .PostActionStatusBar--inPost
+  {
+    padding-top: 0px !important;
+    padding-bottom: 0px !important;
+  }
+
+  div.like_cont.PostBottomActionLikeBtns
+  {
+    border-top: 1px solid #e7e8ec !important;
+  }
+
+  [dir] .wl_post_actions_wrap .like_wrap .PostButtonReactionsContainer
+  {
+    width: 90px !important;
+  }
+
+  .PostButtonReactionsContainer
+  {
+    width: auto !important;
+  }
+
+  .PostButtonReactions__icon.PostButtonReactions__icon--custom
+  {
+    background-image: url(data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22m0%200h24v24h-24z%22%20fill%3D%22none%22%2F%3E%3Cpath%20d%3D%22m17%202.9a6.43%206.43%200%200%201%206.4%206.43c0%203.57-1.43%205.36-7.45%2010l-2.78%202.16a1.9%201.9%200%200%201%20-2.33%200l-2.79-2.12c-6.05-4.68-7.45-6.47-7.45-10.04a6.43%206.43%200%200%201%206.4-6.43%205.7%205.7%200%200%201%205%203.1%205.7%205.7%200%200%201%205-3.1z%22%20fill%3D%22%23ff3347%22%2F%3E%3C%2Fsvg%3E) !important;
+  }
+
+  div.PostButtonReactions__icon.PostButtonReactions__icon--custom.PostButtonReactions__icon--animationActive
+  {
+    background-image: url(data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22m0%200h24v24h-24z%22%20fill%3D%22none%22%2F%3E%3Cpath%20d%3D%22m17%202.9a6.43%206.43%200%200%201%206.4%206.43c0%203.57-1.43%205.36-7.45%2010l-2.78%202.16a1.9%201.9%200%200%201%20-2.33%200l-2.79-2.12c-6.05-4.68-7.45-6.47-7.45-10.04a6.43%206.43%200%200%201%206.4-6.43%205.7%205.7%200%200%201%205%203.1%205.7%205.7%200%200%201%205-3.1z%22%20fill%3D%22%23ff3347%22%2F%3E%3C%2Fsvg%3E) !important;
+  }
+
+  [dir=ltr] .PostButtonReactions__iconAnimation
+  {
+    visibility: hidden;
+  }
+
+  div.PostButtonReactions__icon.PostButtonReactions__icon--custom.PostButtonReactions__icon--animationActive::after
+  {
+    background: url(data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22m0%200h24v24h-24z%22%20fill%3D%22none%22%2F%3E%3Cpath%20d%3D%22m17%202.9a6.43%206.43%200%200%201%206.4%206.43c0%203.57-1.43%205.36-7.45%2010l-2.78%202.16a1.9%201.9%200%200%201%20-2.33%200l-2.79-2.12c-6.05-4.68-7.45-6.47-7.45-10.04a6.43%206.43%200%200%201%206.4-6.43%205.7%205.7%200%200%201%205%203.1%205.7%205.7%200%200%201%205-3.1z%22%20fill%3D%22%23ff3347%22%2F%3E%3C%2Fsvg%3E) !important;
+    visibility: visible;
+  }
+
+  div.PostButtonReactions__icon.PostButtonReactions__icon--custom.PostButtonReactions__icon--animationActive
+  {
+    background: url(data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D%220%200%2024%2024%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22m0%200h24v24h-24z%22%20fill%3D%22none%22%2F%3E%3Cpath%20d%3D%22m17%202.9a6.43%206.43%200%200%201%206.4%206.43c0%203.57-1.43%205.36-7.45%2010l-2.78%202.16a1.9%201.9%200%200%201%20-2.33%200l-2.79-2.12c-6.05-4.68-7.45-6.47-7.45-10.04a6.43%206.43%200%200%201%206.4-6.43%205.7%205.7%200%200%201%205%203.1%205.7%205.7%200%200%201%205-3.1z%22%20fill%3D%22%23ff3347%22%2F%3E%3C%2Fsvg%3E)!important;
+  }
+
+  [dir=ltr] .post--withPostBottomAction .PostBottomActionLikeBtns .like_btns
+  {
+    margin-top: 5px !important;
+  }
+
+  [dir] .PostBottomAction::before
+  {
+    background-image: none!important;
+  }
+
+  div.tt_w.like_tt.likes_tt_share.tt_down
+  {
+    display: none !important;
+  }
+
+  [dir] .like_cont
+  {
+    padding: 0px !important;
+  }
+
+  [dir] .PostBottomActionLikeBtns.like_cont
+  {
+    margin: 10px 4px 0px 4px !important;
+  }
+
+  [dir] .PostBottomActionLikeBtns.like_cont
+  {
+    padding: 3px 0px 0px 0px !important;
+  }
+
+  [dir=ltr] .post--withPostBottomAction .PostBottomActionLikeBtns .like_btns
+  {
+    margin-top: 5px !important;
+  }
+
+  .like_btns
+  {
+    margin-top: 15px !important;
+  }
+
+  [dir] .like_wrap
+  {
+    padding: 0px 20px 8px 20px !important;
+  }
+
+  .like_views
+  {
+    margin-top: 4px !important;
+  }
+  /* Убирает шторку в новых сообщениях */
+ul.ConvoRecommendList {
+display: none;
+}
+/* Кнопка Сервисы VK */
+#react_rootEcosystemServicesNavigationEntry {
+display: none;
 }
 ` );
 
